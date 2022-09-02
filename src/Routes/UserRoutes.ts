359a9fd486da4cb135f0b2b782e45d8609824901
Pedro Coelho19 <pedrocoelho209@gmail.com>
2022-09-02 15:16:13 -0300

    .

---
+++ src/Routes/UserRoutes.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import UsersController from '../controllers/UsersController'
+
+const router = express.Router()
+
+router.post('/user', UsersController.create)
+
+router.get('/user', UsersController.findAll)
+
+router.get('/user/:userId', UsersController.findOne)
+
+router.put('/user/:userId', UsersController.update)
+
+router.delete('/user/:userId', UsersController.destroy)
+
+export { router }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { db } from './database/db'
-import { router } from './routes'
+import { router } from './Routes/UserRoutes'
 
 const app = express()
 app.use(express.json())
